a7f99df6f39e284be35ecf67dad0240a258fc322
Youngseok Ji <jiys5164@naver.com>
2016-11-28 14:25:12 +0900

    TaskActivity UI 수정 및 1시간이 넘을때 시간으로 표현안되던 현상 수정

--- MyLifeLogger/app/src/main/java/jiyoungseok/mylifelogger/TaskActivity.java
+++ MyLifeLogger/app/src/main/java/jiyoungseok/mylifelogger/TaskActivity.java
@@ -9,15 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class TaskActivity extends AppCompatActivity {
 
     Button buttonStart, buttonStop, buttonReset, buttonSave;
     TextView textViewTodayDate, textViewWhatToDo;
     TextView textViewStudy, textViewWork, textViewHobby, textViewWorkout, textViewDate, textViewMove, textViewOther;
+    LinearLayout startStudy, startWork, startHobby, startWorkout, startDate, startMove, startOther;
     Chronometer chronometer;
 
     final TaskDBManager dbManager = new TaskDBManager(this, "task.db", null, 1);
@@ -89,35 +90,43 @@ public class TaskActivity extends AppCompatActivity {
                 currentTime = Integer.parseInt(chronometer.getText().toString().substring(0,2) + chronometer.getText().toString().substring(3));
                 currentTimeToSeconds = ((currentTime / 100) * SECONDS_PER_MINUTE) + (currentTime % 100);
 
-                switch(switchWhatToDo) {
-                    case 1:
-                        dbManager.insert(convertDate, "공부", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 2:
-                        dbManager.insert(convertDate, "직장", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 3:
-                        dbManager.insert(convertDate, "취미", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 4:
-                        dbManager.insert(convertDate, "운동", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 5:
-                        dbManager.insert(convertDate, "데이트", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 6:
-                        dbManager.insert(convertDate, "이동", currentTimeToSeconds);
-                        setText();
-                        break;
-                    case 7:
-                        dbManager.insert(convertDate, "기타", currentTimeToSeconds);
-                        setText();
-                        break;
+                if (isTimerRun == false) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    timeWhenStopped = 0;
+                    chronometer.stop();
+
+                    switch (switchWhatToDo) {
+                        case 1:
+                            dbManager.insert(convertDate, "공부", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 2:
+                            dbManager.insert(convertDate, "직장", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 3:
+                            dbManager.insert(convertDate, "취미", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 4:
+                            dbManager.insert(convertDate, "운동", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 5:
+                            dbManager.insert(convertDate, "데이트", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 6:
+                            dbManager.insert(convertDate, "이동", currentTimeToSeconds);
+                            setText();
+                            break;
+                        case 7:
+                            dbManager.insert(convertDate, "기타", currentTimeToSeconds);
+                            setText();
+                            break;
+                    }
+                } else {
+                    Toast.makeText(TaskActivity.this, "타이머를 멈춘 뒤 저장하세요.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -137,6 +146,14 @@ public class TaskActivity extends AppCompatActivity {
 
     public void onClickStart(View view) {
         textViewWhatToDo = (TextView) findViewById(R.id.textView_WhatToDo);
+        startStudy = (LinearLayout) findViewById(R.id.startStudy);
+        startWork = (LinearLayout) findViewById(R.id.startWork);
+        startHobby = (LinearLayout) findViewById(R.id.startHobby);
+        startWorkout = (LinearLayout) findViewById(R.id.startWorkout);
+        startDate = (LinearLayout) findViewById(R.id.startDate);
+        startMove = (LinearLayout) findViewById(R.id.startMove);
+        startOther = (LinearLayout) findViewById(R.id.startOther);
+
         switch(view.getId()) {
             case R.id.startStudy:
                 if(isTimerRun == false) {
@@ -244,108 +261,19 @@ public class TaskActivity extends AppCompatActivity {
         textViewOther = (TextView) findViewById(R.id.textView_Other);
 
         int timeStudy = dbManager.getTime("공부");
-        if (timeStudy / SECONDS_PER_MINUTE < 10) {
-            if (timeStudy % SECONDS_PER_MINUTE < 10) {
-                textViewStudy.setText("0" + Integer.toString(timeStudy / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeStudy % SECONDS_PER_MINUTE));
-            } else {
-                textViewStudy.setText("0" + Integer.toString(timeStudy / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeStudy % SECONDS_PER_MINUTE));
-            }
-         } else {
-            if (timeStudy % SECONDS_PER_MINUTE < 10) {
-                textViewStudy.setText(Integer.toString(timeStudy / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeStudy % SECONDS_PER_MINUTE));
-            } else {
-                textViewStudy.setText(Integer.toString(timeStudy / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeStudy % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeWork = dbManager.getTime("직장");
-        if (timeWork / SECONDS_PER_MINUTE < 10) {
-            if (timeWork % SECONDS_PER_MINUTE < 10) {
-                textViewWork.setText("0" + Integer.toString(timeWork / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeWork % SECONDS_PER_MINUTE));
-            } else {
-                textViewWork.setText("0" + Integer.toString(timeWork / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeWork % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeWork % SECONDS_PER_MINUTE < 10) {
-                textViewWork.setText(Integer.toString(timeWork / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeWork % SECONDS_PER_MINUTE));
-            } else {
-                textViewWork.setText(Integer.toString(timeWork / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeWork % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeHobby = dbManager.getTime("취미");
-        if (timeHobby / SECONDS_PER_MINUTE < 10) {
-            if (timeHobby % SECONDS_PER_MINUTE < 10) {
-                textViewHobby.setText("0" + Integer.toString(timeHobby / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeHobby % SECONDS_PER_MINUTE));
-            } else {
-                textViewHobby.setText("0" + Integer.toString(timeHobby / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeHobby % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeHobby % SECONDS_PER_MINUTE < 10) {
-                textViewHobby.setText(Integer.toString(timeHobby / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeHobby % SECONDS_PER_MINUTE));
-            } else {
-                textViewHobby.setText(Integer.toString(timeHobby / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeHobby % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeWorkout = dbManager.getTime("운동");
-        if (timeWorkout / SECONDS_PER_MINUTE < 10) {
-            if (timeWorkout % SECONDS_PER_MINUTE < 10) {
-                textViewWorkout.setText("0" + Integer.toString(timeWorkout / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeWorkout % SECONDS_PER_MINUTE));
-            } else {
-                textViewWorkout.setText("0" + Integer.toString(timeWorkout / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeWorkout % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeWorkout % SECONDS_PER_MINUTE < 10) {
-                textViewWorkout.setText(Integer.toString(timeWorkout / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeWorkout % SECONDS_PER_MINUTE));
-            } else {
-                textViewWorkout.setText(Integer.toString(timeWorkout / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeWorkout % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeDate = dbManager.getTime("데이트");
-        if (timeDate / SECONDS_PER_MINUTE < 10) {
-            if (timeDate % SECONDS_PER_MINUTE < 10) {
-                textViewDate.setText("0" + Integer.toString(timeDate / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeDate % SECONDS_PER_MINUTE));
-            } else {
-                textViewDate.setText("0" + Integer.toString(timeDate / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeDate % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeDate % SECONDS_PER_MINUTE < 10) {
-                textViewDate.setText(Integer.toString(timeDate / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeDate % SECONDS_PER_MINUTE));
-            } else {
-                textViewDate.setText(Integer.toString(timeDate / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeDate % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeMove = dbManager.getTime("이동");
-        if (timeMove / SECONDS_PER_MINUTE < 10) {
-            if (timeMove % SECONDS_PER_MINUTE < 10) {
-                textViewMove.setText("0" + Integer.toString(timeMove / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeMove % SECONDS_PER_MINUTE));
-            } else {
-                textViewMove.setText("0" + Integer.toString(timeMove / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeMove % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeMove % SECONDS_PER_MINUTE < 10) {
-                textViewMove.setText(Integer.toString(timeMove / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeMove % SECONDS_PER_MINUTE));
-            } else {
-                textViewMove.setText(Integer.toString(timeMove / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeMove % SECONDS_PER_MINUTE));
-            }
-        }
-
         int timeOther = dbManager.getTime("기타");
-        if (timeOther / SECONDS_PER_MINUTE < 10) {
-            if (timeOther % SECONDS_PER_MINUTE < 10) {
-                textViewOther.setText("0" + Integer.toString(timeOther / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeOther % SECONDS_PER_MINUTE));
-            } else {
-                textViewOther.setText("0" + Integer.toString(timeOther / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeOther % SECONDS_PER_MINUTE));
-            }
-        } else {
-            if (timeOther % SECONDS_PER_MINUTE < 10) {
-                textViewOther.setText(Integer.toString(timeOther / SECONDS_PER_MINUTE) + ":0" + Integer.toString(timeOther % SECONDS_PER_MINUTE));
-            } else {
-                textViewOther.setText(Integer.toString(timeOther / SECONDS_PER_MINUTE) + ":" + Integer.toString(timeOther % SECONDS_PER_MINUTE));
-            }
-        }
+
+        textViewStudy.setText(Integer.toString(timeStudy / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeStudy % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeStudy % SECONDS_PER_MINUTE) + "초");
+        textViewWork.setText(Integer.toString(timeWork / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeWork % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeWork % SECONDS_PER_MINUTE) + "초");
+        textViewHobby.setText(Integer.toString(timeHobby / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeHobby % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeHobby % SECONDS_PER_MINUTE) + "초");
+        textViewWorkout.setText(Integer.toString(timeWorkout / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeWorkout % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeWorkout % SECONDS_PER_MINUTE) + "초");
+        textViewDate.setText(Integer.toString(timeDate / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeDate % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeDate % SECONDS_PER_MINUTE) + "초");
+        textViewMove.setText(Integer.toString(timeMove / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeMove % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeMove % SECONDS_PER_MINUTE) + "초");
+        textViewOther.setText(Integer.toString(timeOther / SECONDS_PER_HOUR) + "시간 " + Integer.toString((timeOther % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE) + "분 " + Integer.toString(timeOther % SECONDS_PER_MINUTE) + "초");
     }
 }
