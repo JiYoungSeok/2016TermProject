1fba236d985f90a5c32b7012b3b14146d93c103f
Youngseok Ji <jiys5164@naver.com>
2016-11-28 22:31:35 +0900

    LocationListener 추가

---
+++ MyLifeLogger/app/src/main/java/jiyoungseok/mylifelogger/GpsInfo.java
@@ -0,0 +1,113 @@
+package jiyoungseok.mylifelogger;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class GpsInfo extends Service implements LocationListener{
+
+    private final Context mContext;
+
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean isGetLocation = false;
+
+    Location location;
+
+    double latitude;
+    double longitude;
+
+    private static final long MIN_DISTANCE_UPDATES = 10;
+    private static final long MIN_TIME_UPDATES = 1000 * 60 * 1;
+
+    protected LocationManager locationManager;
+
+    public GpsInfo(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+            } else {
+                this.isGetLocation = true;
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_UPDATES, MIN_DISTANCE_UPDATES, this);
+
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_UPDATES, MIN_DISTANCE_UPDATES, this);
+
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    public boolean isGetLocation() {
+        return this.isGetLocation;
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    public void onLocationChanged(Location location) {
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    public void onProviderEnabled(String provider) {
+    }
+
+    public void onProviderDisabled(String provider) {
+    }
+}
--- MyLifeLogger/app/src/main/java/jiyoungseok/mylifelogger/LogActivity.java
+++ MyLifeLogger/app/src/main/java/jiyoungseok/mylifelogger/LogActivity.java
@@ -1,17 +1,65 @@
 package jiyoungseok.mylifelogger;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
+
+    private double currentlat, currentlon;
+    TextView textViewLatitude, textViewLongitude;
+    Button buttonCheckLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("GPS를 켜야 어플이 작동됩니다. GPS를 켜고 어플을 재시작 해주시기 바랍니다.").setCancelable(false).setPositiveButton("GPS 켜기", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent gpsOptionIntent = new Intent (Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(gpsOptionIntent);
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+        init();
+
+        textViewLatitude = (TextView) findViewById(R.id.textView_Latitude);
+        textViewLongitude = (TextView) findViewById(R.id.textView_Longitude);
+
+        buttonCheckLocation = (Button) findViewById(R.id.button_CheckLocation);
+
+        buttonCheckLocation.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewLatitude.setText(Double.toString(currentlat));
+                textViewLongitude.setText(Double.toString(currentlon));
+            }
+        });
+    }
+
+    public void init() {
+        GpsInfo gps = new GpsInfo(this);
+
+        if (gps.isGetLocation()) {
+            currentlat = gps.getLatitude();
+            currentlon = gps.getLongitude();
+        }
     }
 
     public void onClickChangePage(View view) {
